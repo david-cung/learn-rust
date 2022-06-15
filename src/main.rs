47b38d2b32ca6c1f2a3c5f9e467644301083c07a
david-cung <vancung1951997@gmail.com>
2022-06-15 23:18:53 +0700

    exercise 1

---
+++ src/main.rs
@@ -0,0 +1,18 @@
+fn main() {
+    
+    let x = change_value(10,&mut 20);
+    print!("x: {}", x)
+}
+
+
+
+fn change_value(input:u32, output: &mut u32) -> u32{
+    if input ==1 {
+        *output =3;
+    }
+    else {
+        *output = 4;
+    }
+
+    *output
+}
